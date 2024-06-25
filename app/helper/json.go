bfbc14fba6d7d5f4ff952e0d257c5b09b4d0a80f
faizdotid <faizalnurpratama76@gmail.com>
2024-06-25 19:28:52 +0700

    fix: add sta

--- app/helper/json.go
+++ app/helper/json.go
@@ -8,5 +8,7 @@ import (
 // write json response using map
 // helper.JSON(w, http.StatusOK, map[string]string{"message": "pong"})
 func JSON(w http.ResponseWriter, code int, message map[string]interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
 	json.NewEncoder(w).Encode(message)
 }
