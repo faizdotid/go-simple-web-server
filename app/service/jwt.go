99678c630e281432691a4647534d7b76184985d4
faizdotid <faizalnurpratama76@gmail.com>
2024-06-30 21:01:52 +0700

    fix: exp time jwt

--- app/service/jwt.go
+++ app/service/jwt.go
@@ -2,8 +2,10 @@
 package service
 
 import (
+	"fmt"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
@@ -12,13 +14,15 @@ var jwtSecret = os.Getenv("JWT_SECRET")
 
 // EncrytData encrypts data and returns a token
 func EncrytData(data map[string]interface{}) (string, error) {
-	service := jwt.New(jwt.SigningMethodHS256)
-	claims := service.Claims.(jwt.MapClaims)
 	jwtExp, _ := strconv.Atoi(os.Getenv("JWT_EXP"))
-	claims["exp"] = jwtExp
-	for key, value := range data {
-		claims[key] = value
-	}
+	service := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"exp":  time.Now().Add(time.Hour * time.Duration(jwtExp)).Unix(),
+			"iat":  time.Now().Unix(),
+			"data": data,
+		},
+	)
 	token, err := service.SignedString([]byte(jwtSecret))
 	if err != nil {
 		return "", err
@@ -30,13 +34,17 @@ func EncrytData(data map[string]interface{}) (string, error) {
 func DecryptData(token string) (map[string]interface{}, error) {
 	service, err := jwt.Parse(
 		token,
-		func(token *jwt.Token) (interface{}, error) {
+		func(t *jwt.Token) (interface{}, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+			}
 			return []byte(jwtSecret), nil
 		},
 	)
+
 	if err != nil {
 		return nil, err
 	}
-	claims := service.Claims.(jwt.MapClaims)
+	claims := service.Claims.(jwt.MapClaims)["data"].(map[string]interface{})
 	return claims, nil
 }
